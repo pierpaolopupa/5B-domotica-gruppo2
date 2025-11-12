6af5c0fe47c6bd63916b45f4e7ac3c6fe2282ca1
Luca <5B_INF_PUPA@CENTROSTUDI.local>
2025-11-12 10:55:27 +0100

    [LUCA] Fix pom.xml \n Publish released first artifact

---
+++ src/main/java/api/Start.java
@@ -0,0 +1,21 @@
+package api;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class Start {
+	
+	private static final Logger log = LogManager.getLogger(Start.class);
+	
+	public void load() {
+		
+	}
+	
+	public void start() {
+		
+	}
+	
+	public void shutdown() {
+		
+	}
+}
---
+++ src/main/java/it/gruppo2b/domotica/Serenya.java
@@ -0,0 +1,15 @@
+package it.gruppo2b.domotica;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class Serenya {
+	
+	private static final Logger log = LogManager.getLogger(Serenya.class);
+	
+	public static void main(String[] args) {
+		
+		log.info("Avvio...");
+		
+	}
+}
